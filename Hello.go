d66b7c79f1e4328a5b6739e69c21385f40a5c7c5
main <markxkamau@gmail.com>
2024-02-16 22:02:05 +0100

    First Commit: Start of GoLang

---
+++ Hello.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+var (
+	name string
+	age int
+	year int
+)
+
+func main() {
+	var pntr *string
+	greeting := "Greetings from "+name
+
+	pntr = &greeting
+
+	fmt.Println(*pntr)
+	fmt.Printf("%v was born in the year %v and is currently %v years old",name, year,age)
+}
